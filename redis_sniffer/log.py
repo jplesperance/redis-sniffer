25f4ed4421b723703458c90b76db55b3d1ac718a
= <jesse@jplesperance.me>
2015-03-18 17:51:47 -0700

    Added a funtion to log a command into it's own file

--- redis_sniffer/log.py
+++ redis_sniffer/log.py
@@ -2,13 +2,18 @@ import io
 
 
 class Log:
-    def __init__(self, log_level="event", location=None, files_names={}):
+    def __init__(self, log_level="event", location=None, files_names={}, filters={}, append="_sniff"):
         if log_level == "event" or log_level == "both" or log_level == "debug":
             self.event_log = io.open(location + files_names['event'], 'w')
         elif log_level == "full" or log_level == "both" or log_level == "debug":
             self.full_log = io.open(location + files_names['full'], 'w')
         elif log_level == "debug":
             self.debug_log = io.open(location + files_names['debug'], 'w')
+        self.files = {}
+        if len(filters) > 0:
+            for event in filters:
+                self.files[event] = io.open(location + event + append)
+
 
     def write_event(self, event):
         self.event_log.write(event)
@@ -21,3 +26,8 @@ class Log:
     def write_debug(self, data):
         self.debug_log.write(data)
         self.debug_log.flush()
+
+    def write_command(self, event, command):
+        if event in self.files.keys():
+            self.files[event].write(command)
+            self.files[event].flush()
