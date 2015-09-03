e903826eab6d6a1d13760278e8958c54ca4125af
Jesse Lesperance <jlesperance@apixio.com>
2015-09-03 16:53:36 -0700

    updating tests

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 import unittest
 from redis_sniffer import sniffer
 
- class TestClass(unittest.TestCase):
-     def test_version(self):
+class TestClass(unittest.TestCase):
+    def test_version(self):
         self.assertEqual(sniffer.version(), "v1.0.0")
 
 
